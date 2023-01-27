a888d8be543b37efe73095134f3ac66af0d51862
Ranveer Raj <ranveerrajriteshraushan@gmail.com>
2023-01-27 16:49:27 +0530

    Update segtree.cpp
    Added input output path by default

--- segtree.cpp
+++ segtree.cpp
@@ -157,15 +157,15 @@ void solve()
 
 signed main()
 {
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    // freopen("error.txt", "w", stderr);
-    // #endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    // int _t=1;cin>>_t;while(_t--)
+    int _t=1;cin>>_t;while(_t--)
     solve();
     return 0;
 }
