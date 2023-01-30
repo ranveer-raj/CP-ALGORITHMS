e2e33b2d26ead6a17aeeafca4970ce6b64c6d79b
Ranveer Raj <ranveerrajriteshraushan@gmail.com>
2023-01-30 22:02:27 +0530

    Create Sieve_of_eratosthenesis.cpp
    Sieve of Eratosthenesis

---
+++ Sieve_of_eratosthenesis.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int , null_type, less<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_set;
+typedef tree<int , null_type ,  less_equal<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_multiset;
+// value at ith index can be found by --> *st.find_by_order(i)
+// index of number (num) can be found by --> st.order_of_key(num)
+// if number (num) is not in the set then it will show the last index number i.e. st.end()
+#endif
+
+#define int long long
+#define double long double
+#define space " "
+#define endl "\n"
+#define intmin LLONG_MIN
+#define intmax LLONG_MAX
+#define infinity ULLONG_MAX
+#define bitcount(x) __builtin_popcountll(x)
+#define setbit(x,i) (x|(1LL<<i))
+#define unsetbit(x,i) (x&(~(1LL<<i)))
+#define flipbit(x,i) (x^(1LL<<i))
+#define checkbit(x,i) ((x>>i)&1LL)
+#define msb(x) (63-(__builtin_clzll(x)))
+#define lsb(x) ((__builtin_ctzll(x)))
+#define itob(x) bitset<64>(x).to_string()
+
+#ifndef ONLINE_JUDGE
+#define debarr(a,n)cerr<<#a<<":";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl;
+#define debmat(mat,row,col)cerr<<#mat<<":\n";for(int i=0;i<row;i++){for(int j=0;j<col;j++)cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...)dbs(#__VA_ARGS__,__VA_ARGS__)
+template<class S,class T>ostream &operator<<(ostream &os,const pair<S,T> &p){return os<<"("<<p.first<<","<<p.second<<")";}
+template<class T>ostream &operator<<(ostream &os,const vector<T> &p){os<<"[";for(auto&it:p)os<<it<<" ";return os<<"]";}
+template<class T>ostream &operator<<(ostream &os,const set<T>&p){os<<"[";for(auto&it:p)os<<it<<" ";return os<<"]";}
+template<class T>ostream &operator<<(ostream &os,const multiset<T>&p){os<<"[";for(auto&it:p)os<<it<<" ";return os<<"]";}
+template<class S,class T>ostream &operator<<(ostream &os,const map<S,T>&p){os<<"[";for(auto&it:p)os<<it<<" ";return os<<"]";}
+template<class T>void dbs(string str,T t){cerr<<str<<":"<<t<<"\n";}
+template<class T,class...S>void dbs(string str,T t,S... s){int idx=str.find(',');cerr<<str.substr(0,idx)<<":"<<t<<",";dbs(str.substr(idx+1),s...);}
+#else
+#define pr(...){}
+#define debarr(a,n){}
+#define debmat(mat,row,col){}
+#endif
+
+const int mod=1000000007;
+const int inf=1e18;
+const double pi=acos(-1.0);
+const int nax1=100001;
+const int nax2=1000001;
+const int nax3=10000001;
+
+/******************** Sieve of eratosthensis start here ********************/
+const int krr=31623;
+vector<int> prime;
+int sp[krr];
+void setSP(){
+    int s=krr;
+    for(int i=0;i<s;i++)
+    {
+        sp[i]=i;
+    }
+    for(int i=2;i*i<s;i++)
+    {
+        if(sp[i]==i)
+        {
+            for(int j=i*i;j<s;j+=i)
+            {
+                if(sp[j]==j)
+                {
+                    sp[j]=i;
+                }
+            }
+        }
+    }
+    for(int i=2;i<s;i++)
+    {
+        if(sp[i]==i)
+        {
+            prime.push_back(i);
+        }
+    }
+}
+/******************** Sieve of eratosthensis end here ********************/
+
+void solve()
+{
+
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    setSP();
+    int _t=1;cin>>_t;while(_t--)
+    solve();
+    return 0;
+}
